fe45c038f70980f03e8a010b5ba919ea45bab486
Vitalii Orazov <viordash@mail.ru>
2026-04-27 23:25:13 +0300

    feat: add IEEE 802.11 Antenna element (RFC 5416 section 6.2)

---
+++ src/elements/Antenna.cpp
@@ -0,0 +1,187 @@
+#include "Antenna.h"
+#include "Logging.h"
+#include "lassert.h"
+#include <algorithm>
+#include <cstring>
+
+Antenna::Antenna(uint8_t radio_id, Diversity diversity, Combiner combiner, uint8_t antenna_count)
+    : ElementHeader(ElementHeader::Antenna,
+                    (sizeof(Antenna) - sizeof(ElementHeader)) + antenna_count),
+      radio_id{ radio_id }, diversity{ diversity }, combiner{ combiner },
+      antenna_count{ antenna_count } {
+}
+
+uint8_t Antenna::GetRadioID() const {
+    return radio_id;
+}
+
+Antenna::Diversity Antenna::GetDiversity() const {
+    return diversity;
+}
+
+Antenna::Combiner Antenna::GetCombiner() const {
+    return combiner;
+}
+
+uint8_t Antenna::GetAntennaCount() const {
+    return antenna_count;
+}
+
+bool Antenna::Validate() const {
+    static_assert(sizeof(Antenna) == 8);
+    if (ElementHeader::GetElementType() != ElementHeader::Antenna) {
+        return false;
+    }
+    auto selection_count = GetLength() - (sizeof(Antenna) - sizeof(ElementHeader));
+    if (selection_count > max_antenna_count) {
+        log_e("Antenna: antenna selection count exceeds limit: %u", (unsigned)max_antenna_count);
+        return false;
+    }
+    if (selection_count != antenna_count) {
+        log_e("Antenna: antenna count mismatch");
+        return false;
+    }
+    if (radio_id > 31) {
+        return false;
+    }
+    switch (diversity) {
+        case Antenna::Diversity::DiversityDisabled:
+        case Antenna::Diversity::DiversityEnabled:
+            break;
+        default:
+            return false;
+    }
+    switch (combiner) {
+        case Antenna::Combiner::SectorizedLeft:
+        case Antenna::Combiner::SectorizedRight:
+        case Antenna::Combiner::Omni:
+        case Antenna::Combiner::MIMO:
+            break;
+        default:
+            return false;
+    }
+    for (size_t i = 0; i < antenna_count; i++) {
+        switch (antenna_selection[i]) {
+            case Antenna::AntennaSelection::Internal:
+            case Antenna::AntennaSelection::External:
+                break;
+            default:
+                return false;
+        }
+    }
+    return true;
+}
+
+void Antenna::Serialize(RawData *raw_data) const {
+    ASSERT(raw_data->current + sizeof(Antenna) <= raw_data->end);
+    Antenna *dst = (Antenna *)raw_data->current;
+    *dst = *this;
+    raw_data->current += sizeof(Antenna);
+}
+
+Antenna *Antenna::Deserialize(RawData *raw_data) {
+    if (raw_data->current + sizeof(Antenna) > raw_data->end) {
+        return nullptr;
+    }
+
+    auto res = (Antenna *)raw_data->current;
+    if (!res->Validate()) {
+        return nullptr;
+    }
+
+    uint8_t *last = raw_data->current + sizeof(ElementHeader) + res->GetLength();
+    if (last > raw_data->end) {
+        return nullptr;
+    }
+
+    raw_data->current = last;
+    return res;
+}
+
+WritableAntennaArray::WritableAntennaArray() {
+    items.reserve(ReadableAntennaArray::max_count);
+}
+
+void WritableAntennaArray::Add(uint8_t radio_id,
+                               Antenna::Diversity diversity,
+                               Antenna::Combiner combiner,
+                               std::vector<Antenna::AntennaSelection> selections) {
+    ASSERT(items.size() + 1 <= ReadableAntennaArray::max_count);
+
+    auto it_exists = std::find_if(items.begin(),
+                                  items.end(),
+                                  [&radio_id](const WritableAntennaArray::Item &item) {
+                                      return item.header.GetRadioID() == radio_id;
+                                  });
+
+    if (it_exists != items.end()) {
+        *it_exists =
+            WritableAntennaArray::Item{ radio_id, diversity, combiner, std::move(selections) };
+        log_i("Antenna: replace RadioID: %u", radio_id);
+    } else {
+        items.emplace_back(radio_id, diversity, combiner, std::move(selections));
+    }
+}
+
+bool WritableAntennaArray::Empty() const {
+    return items.empty();
+}
+
+void WritableAntennaArray::Clear() {
+    items.clear();
+}
+
+void WritableAntennaArray::Serialize(RawData *raw_data) const {
+    for (const auto &elem : items) {
+        elem.header.Serialize(raw_data);
+        auto selection_count =
+            elem.header.GetLength() - (sizeof(Antenna) - sizeof(ElementHeader));
+        memcpy(raw_data->current, elem.selections.data(), selection_count);
+        raw_data->current += selection_count;
+    }
+}
+
+void WritableAntennaArray::Log() const {
+    for (size_t i = 0; i < items.size(); i++) {
+        log_i("ME Antenna #%zu RadioID:%u, Diversity:%u, Combiner:%u, AntennaCount:%u",
+              i,
+              items[i].header.GetRadioID(),
+              items[i].header.GetDiversity(),
+              items[i].header.GetCombiner(),
+              items[i].header.GetAntennaCount());
+    }
+}
+
+ReadableAntennaArray::ReadableAntennaArray() : count{ 0 } {
+}
+
+bool ReadableAntennaArray::Deserialize(RawData *raw_data) {
+    if (count >= max_count) {
+        log_e("ReadableAntennaArray::Deserialize elements count exceeds");
+        return false;
+    }
+
+    auto antenna = Antenna::Deserialize(raw_data);
+    if (antenna == nullptr) {
+        return false;
+    }
+    items[count] = antenna;
+    count++;
+    return true;
+}
+
+nonstd::span<const Antenna *const> ReadableAntennaArray::Get() const {
+    nonstd::span span(items.begin(), count);
+    return span;
+}
+
+void ReadableAntennaArray::Log() const {
+    for (size_t i = 0; i < count; i++) {
+        log_i("ME Antenna #%zu RadioID:%u, Diversity:%u, Combiner:%u, AntennaCount:%u",
+              i,
+              items[i]->GetRadioID(),
+              items[i]->GetDiversity(),
+              items[i]->GetCombiner(),
+              items[i]->GetAntennaCount());
+    }
+}
---
+++ src/elements/Antenna.h
@@ -0,0 +1,101 @@
+#pragma once
+
+#include "Helpers.h"
+#include "elements/ElementHeader.h"
+#include "span.hpp"
+#include <array>
+#include <cstdint>
+#include <vector>
+
+struct __attribute__((packed)) Antenna : ElementHeader {
+  public:
+    static const size_t max_antenna_count = 255;
+
+    enum Diversity : uint8_t { DiversityDisabled = 0, DiversityEnabled = 1 };
+
+    enum Combiner : uint8_t {
+        SectorizedLeft = 1,
+        SectorizedRight = 2,
+        Omni = 3,
+        MIMO = 4
+    };
+
+    enum AntennaSelection : uint8_t { Internal = 1, External = 2 };
+
+  protected:
+    // Radio ID: An 8-bit value representing the radio, whose value is between 0 and 31.
+    uint8_t radio_id;
+
+    // Diversity: An 8-bit value specifying whether the antenna is to provide receiver diversity.
+    Diversity diversity;
+
+    // Combiner: An 8-bit value specifying the combiner selection.
+    Combiner combiner;
+
+    // Antenna Count: An 8-bit value specifying the number of Antenna Selection fields.
+    uint8_t antenna_count;
+
+  public:
+    // Antenna Selection: One 8-bit antenna configuration value per antenna in the WTP.
+    AntennaSelection antenna_selection[];
+
+    Antenna(const Antenna &) = default;
+    Antenna(uint8_t radio_id, Diversity diversity, Combiner combiner, uint8_t antenna_count);
+
+    uint8_t GetRadioID() const;
+    Diversity GetDiversity() const;
+    Combiner GetCombiner() const;
+    uint8_t GetAntennaCount() const;
+    bool Validate() const;
+    void Serialize(RawData *raw_data) const;
+    static Antenna *Deserialize(RawData *raw_data);
+};
+
+struct WritableAntennaArray {
+  public:
+    struct Item {
+        std::vector<Antenna::AntennaSelection> selections;
+        Antenna header;
+        Item(const Item &) = default;
+        Item(uint8_t radio_id,
+             Antenna::Diversity diversity,
+             Antenna::Combiner combiner,
+             std::vector<Antenna::AntennaSelection> &&sel)
+            : selections{ std::move(sel) },
+              header{ radio_id, diversity, combiner, (uint8_t)selections.size() } {};
+    };
+
+  private:
+    std::vector<Item> items;
+
+  public:
+    WritableAntennaArray(const WritableAntennaArray &) = delete;
+    WritableAntennaArray();
+
+    void Add(uint8_t radio_id,
+             Antenna::Diversity diversity,
+             Antenna::Combiner combiner,
+             std::vector<Antenna::AntennaSelection> selections);
+    bool Empty() const;
+    void Clear();
+
+    void Serialize(RawData *raw_data) const;
+    void Log() const;
+};
+
+struct ReadableAntennaArray {
+  public:
+    static const size_t max_count = 32;
+
+  protected:
+    std::array<const Antenna *, max_count> items;
+    size_t count;
+
+  public:
+    ReadableAntennaArray(const ReadableAntennaArray &) = delete;
+    ReadableAntennaArray();
+
+    bool Deserialize(RawData *raw_data);
+    nonstd::span<const Antenna *const> Get() const;
+    void Log() const;
+};
