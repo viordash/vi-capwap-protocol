35b484d5b0aaca58891b7f169b0453af523b6623
Vitalii Orazov <viordash@mail.ru>
2026-05-04 15:07:54 +0300

    chore: code format

--- src/ChangeStateEventResponse.h
+++ src/ChangeStateEventResponse.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#include "IElement.h"
 #include "CapwapMessage.h"
+#include "IElement.h"
 #include "elements/VendorSpecificPayload.h"
 #include "span.hpp"
 #include <limits>
@@ -9,14 +9,15 @@
 
 struct WritableChangeStateEventResponse : WritableCapwapResponse {
   private:
-    nonstd::span<IWritableChangeStateEventResponseOptionalElement  *const> optional_elements;
+    nonstd::span<IWritableChangeStateEventResponseOptionalElement *const> optional_elements;
 
   public:
     WritableChangeStateEventResponse(const WritableChangeStateEventResponse &) = delete;
     WritableChangeStateEventResponse(
-        nonstd::span<IWritableChangeStateEventResponseOptionalElement  *const> optional_elements);
+        nonstd::span<IWritableChangeStateEventResponseOptionalElement *const> optional_elements);
     WritableChangeStateEventResponse(
-        std::initializer_list<IWritableChangeStateEventResponseOptionalElement  *const> optional_elements);
+        std::initializer_list<IWritableChangeStateEventResponseOptionalElement *const>
+            optional_elements);
 
     ControlHeader::MessageType GetMessageType() const override final;
     ControlHeader::MessageType GetRequestMessageType() const override final;
@@ -25,22 +26,24 @@ struct WritableChangeStateEventResponse : WritableCapwapResponse {
 
 struct ReadableChangeStateEventResponse : ReadableCapwapResponse {
   protected:
-    std::unordered_map<ElementHeader::ElementType, IReadableChangeStateEventResponseOptionalElement  *const>
+    std::unordered_map<ElementHeader::ElementType,
+                       IReadableChangeStateEventResponseOptionalElement *const>
         key_optional_elements;
 
-    std::unordered_map<ElementHeader::ElementType, IReadableChangeStateEventResponseOptionalElement  *const>
+    std::unordered_map<ElementHeader::ElementType,
+                       IReadableChangeStateEventResponseOptionalElement *const>
     MapOptionalsElements(
-        nonstd::span<IReadableChangeStateEventResponseOptionalElement  *const> optional_elements);
+        nonstd::span<IReadableChangeStateEventResponseOptionalElement *const> optional_elements);
 
   public:
-
     size_t unknown_elements;
 
     ReadableChangeStateEventResponse(const ReadableChangeStateEventResponse &) = delete;
     ReadableChangeStateEventResponse(
-        nonstd::span<IReadableChangeStateEventResponseOptionalElement  *const> optional_elements);
+        nonstd::span<IReadableChangeStateEventResponseOptionalElement *const> optional_elements);
     ReadableChangeStateEventResponse(
-        std::initializer_list<IReadableChangeStateEventResponseOptionalElement  *> optional_elements);
+        std::initializer_list<IReadableChangeStateEventResponseOptionalElement *>
+            optional_elements);
 
     ControlHeader::MessageType GetMessageType() const override final;
     bool Deserialize(RawData *raw_data) override final;
