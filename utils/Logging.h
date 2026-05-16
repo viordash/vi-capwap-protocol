a42f26d543bb3017b21915abe663982fe720ff96
Vitalii Orazov <viordash@mail.ru>
2026-05-16 17:51:58 +0300

    feat: switch Logging.h to fmt-style {} macros with spdlog/fmt auto-detection

--- utils/Logging.h
+++ utils/Logging.h
@@ -1,12 +1,5 @@
 #pragma once
 
-#include <errno.h>
-#include <inttypes.h>
-#include <stdint.h>
-#include <stdio.h>
-
-const char *log_time();
-
 #define LOG_LEVEL_NONE 4
 #define LOG_LEVEL_ERROR 3
 #define LOG_LEVEL_WARN 2
@@ -23,52 +16,96 @@ typedef enum {
 
 #ifndef LOG_LEVEL
 #define LOG_LEVEL LOG_LEVEL_DEBUG
-#endif // !LOG_LEVEL
-
-#if (LOG_LEVEL <= LOG_LEVEL_ERROR)
-#define log_e(format, ...) fprintf(stderr, "E (%s) " format "\n", log_time(), ##__VA_ARGS__)
-#else
-#define log_e(format, ...)                                                                         \
-    do {                                                                                           \
-        if (0) {                                                                                   \
-            fprintf(stderr, format, ##__VA_ARGS__);                                                \
-        }                                                                                          \
-    } while (0)
-#endif
-
-#if (LOG_LEVEL <= LOG_LEVEL_WARN)
-#define log_w(format, ...) fprintf(stderr, "W (%s) " format "\n", log_time(), ##__VA_ARGS__)
-#else
-#define log_w(format, ...)                                                                         \
-    do {                                                                                           \
-        if (0) {                                                                                   \
-            fprintf(stderr, format, ##__VA_ARGS__);                                                \
-        }                                                                                          \
-    } while (0)
-#endif
-
-#if (LOG_LEVEL <= LOG_LEVEL_INFO)
-#define log_i(format, ...) fprintf(stdout, "I (%s) " format "\n", log_time(), ##__VA_ARGS__)
-#else
-#define log_i(format, ...)                                                                         \
-    do {                                                                                           \
-        if (0) {                                                                                   \
-            fprintf(stdout, format, ##__VA_ARGS__);                                                \
-        }                                                                                          \
-    } while (0)
-#endif
-
-#if (LOG_LEVEL <= LOG_LEVEL_DEBUG)
-#define log_d(format, ...) fprintf(stdout, "D (%s) " format "\n", log_time(), ##__VA_ARGS__)
-#else
-#define log_d(format, ...)                                                                         \
-    do {                                                                                           \
-        if (0) {                                                                                   \
-            fprintf(stdout, format, ##__VA_ARGS__);                                                \
-        }                                                                                          \
-    } while (0)
 #endif
 
 static inline void log_level_set(log_level_t level) {
     (void)level;
 }
+
+#ifndef log_i
+
+#if __has_include(<spdlog/spdlog.h>)
+
+#if (LOG_LEVEL == LOG_LEVEL_NONE)
+#ifndef SPDLOG_ACTIVE_LEVEL
+#define SPDLOG_ACTIVE_LEVEL SPDLOG_LEVEL_OFF
+#endif
+#elif (LOG_LEVEL == LOG_LEVEL_ERROR)
+#ifndef SPDLOG_ACTIVE_LEVEL
+#define SPDLOG_ACTIVE_LEVEL SPDLOG_LEVEL_ERROR
+#endif
+#elif (LOG_LEVEL == LOG_LEVEL_WARN)
+#ifndef SPDLOG_ACTIVE_LEVEL
+#define SPDLOG_ACTIVE_LEVEL SPDLOG_LEVEL_WARN
+#endif
+#elif (LOG_LEVEL == LOG_LEVEL_INFO)
+#ifndef SPDLOG_ACTIVE_LEVEL
+#define SPDLOG_ACTIVE_LEVEL SPDLOG_LEVEL_INFO
+#endif
+#else
+#ifndef SPDLOG_ACTIVE_LEVEL
+#define SPDLOG_ACTIVE_LEVEL SPDLOG_LEVEL_DEBUG
+#endif
+#endif
+
+#include <spdlog/spdlog.h>
+
+#if (LOG_LEVEL <= LOG_LEVEL_ERROR)
+#define log_e(fmt_, ...) SPDLOG_ERROR(fmt_, ##__VA_ARGS__)
+#else
+#define log_e(fmt_, ...) (void)0
+#endif
+
+#if (LOG_LEVEL <= LOG_LEVEL_WARN)
+#define log_w(fmt_, ...) SPDLOG_WARN(fmt_, ##__VA_ARGS__)
+#else
+#define log_w(fmt_, ...) (void)0
+#endif
+
+#if (LOG_LEVEL <= LOG_LEVEL_INFO)
+#define log_i(fmt_, ...) SPDLOG_INFO(fmt_, ##__VA_ARGS__)
+#else
+#define log_i(fmt_, ...) (void)0
+#endif
+
+#if (LOG_LEVEL <= LOG_LEVEL_DEBUG)
+#define log_d(fmt_, ...) SPDLOG_DEBUG(fmt_, ##__VA_ARGS__)
+#else
+#define log_d(fmt_, ...) (void)0
+#endif
+
+#elif __has_include(<fmt/format.h>)
+
+#include <fmt/format.h>
+
+const char *log_time();
+
+#if (LOG_LEVEL <= LOG_LEVEL_ERROR)
+#define log_e(fmt_, ...) fmt::print(stderr, "E ({}) " fmt_ "\n", log_time(), ##__VA_ARGS__)
+#else
+#define log_e(fmt_, ...) (void)0
+#endif
+
+#if (LOG_LEVEL <= LOG_LEVEL_WARN)
+#define log_w(fmt_, ...) fmt::print(stderr, "W ({}) " fmt_ "\n", log_time(), ##__VA_ARGS__)
+#else
+#define log_w(fmt_, ...) (void)0
+#endif
+
+#if (LOG_LEVEL <= LOG_LEVEL_INFO)
+#define log_i(fmt_, ...) fmt::print(stdout, "I ({}) " fmt_ "\n", log_time(), ##__VA_ARGS__)
+#else
+#define log_i(fmt_, ...) (void)0
+#endif
+
+#if (LOG_LEVEL <= LOG_LEVEL_DEBUG)
+#define log_d(fmt_, ...) fmt::print(stdout, "D ({}) " fmt_ "\n", log_time(), ##__VA_ARGS__)
+#else
+#define log_d(fmt_, ...) (void)0
+#endif
+
+#else
+#error "capwap-protocol logging requires either spdlog or libfmt (apt install libfmt-dev)"
+#endif
+
+#endif // !log_i
